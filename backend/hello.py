ebe1b138cf20f670c4bd1c56d79ed4f875e4565b
mihir jajodia <mihirjajodia51@gmail.com>
2024-03-12 23:13:08 +0100

    flask connection to postgresql

--- backend/hello.py
+++ backend/hello.py
@@ -1,14 +1,29 @@
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/products'
+db = SQLAlchemy(app)
 CORS(app)
 
 cross_origin(supports_credentials=True)
 
+class Product(db.Model):
+    __tablename__ = 'products'  
+    id = db.Column(db.Integer, primary_key=True)
+    productname = db.Column(db.String(80), nullable=False)
+
+
 @app.route('/test')
 def test_endpoint():
-    return 'Hello World'
+    all_products = Product.query.all()
+    products_list = []
+    for product in all_products:
+        product_data = {'id': product.id, 'productname': product.productname}
+        products_list.append(product_data)
+    return jsonify(products_list)
 
 @app.route('/secondtest', methods=[('GET')])
 def test_second_endpoint():
